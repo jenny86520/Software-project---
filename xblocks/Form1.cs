925702e51da09389f6d507d72a6f4844ba7f41fc
jenny86520 <32293270+jenny86520@users.noreply.github.com>
2018-01-06 18:05:49 +0800

    initial file
    initial file

---
+++ xblocks/Form1.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using xblocks;
+
+namespace banana
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 f = new Form2();//產生Form2的物件，才可以使用它所提供的Method
+
+            this.Visible = false;//將Form1隱藏。由於在Form1的程式碼內使用this，所以this為Form1的物件本身
+            f.Visible = true;//顯示第二個視窗
+        }
+    }
+}
