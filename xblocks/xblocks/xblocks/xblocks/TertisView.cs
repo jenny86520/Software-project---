4f7d5afc5f556f95cad7b94cd54e2776930249b7
jenny86520 <32293270+jenny86520@users.noreply.github.com>
2018-01-10 17:05:10 +0800

    V9.0
    新增、編輯view、controllar檔案

---
+++ xblocks/xblocks/xblocks/xblocks/TertisView.cs
@@ -0,0 +1,100 @@
+﻿using banana;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace xblocks
+{
+    public partial class TertisView : Form
+    {
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.P)
+            {
+                if (game_mode == 0) { game_mode = 1; timer1.Enabled = true; }
+                else { game_mode = 0; timer1.Enabled = false; }
+            }
+
+            if (e.KeyCode == Keys.Left)
+            {
+                if (x_direction(block_type, block_row, block_col, -1))
+                {
+                    block_col_pre = block_col; block_col--;
+                    block_changed = true;
+                }
+            }
+
+            if (e.KeyCode == Keys.Right)
+            {
+                if (x_direction(block_type, block_row, block_col, 1))
+                {
+                    block_col_pre = block_col; block_col++;
+                    block_changed = true;
+                }
+            }
+
+            if (e.KeyCode == Keys.Up)
+            {
+                block_type_pre = block_type;
+                block_col_pre = block_col; block_row_pre = block_row;
+                block_type = next_block_type(block_type, block_row, block_col);
+                if (block_type != block_type_pre)
+                    block_changed = true;
+            }
+
+            if (e.KeyCode == Keys.S)
+            {
+                game_mode = 2;
+                timer_interval -= 50;
+
+                if (timer_interval <= 0)
+                    timer_interval = 1;
+
+                timer1.Interval = timer_interval;
+                label_level.Text = "Level:" + (1010 - timer_interval) / 50;
+            }
+
+            if (e.KeyCode == Keys.A)
+            {
+                game_mode = 2;
+                timer_interval += 50;
+
+                if (timer_interval >= 1010)
+                    timer_interval = 1010;
+
+                timer1.Interval = timer_interval;
+                label_level.Text = "Level:" + (1010 - timer_interval) / 50;
+            }
+
+            if (e.KeyCode == Keys.Down)
+                timer1.Interval = 15;
+
+
+            if (block_changed)
+            {
+                erase_block(block_row_pre, block_col_pre, block_type_pre);
+                update_block(block_row, block_col, block_type);
+                show_grids();
+                block_row_pre = block_row; block_col_pre = block_col; block_type_pre = block_type;
+                block_changed = false;
+            }
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            TertisModel f = new TertisModel();//產生Form2的物件，才可以使用它所提供的Method
+
+            this.Visible = false;//將Form1隱藏。由於在Form1的程式碼內使用this，所以this為Form1的物件本身
+            f.Visible = true;//顯示第二個視窗
+        }
+
+        
+    }
+}
